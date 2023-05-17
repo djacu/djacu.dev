7f404324f1b1954137d4d714d23b9749100b6137
Dan Baker <daniel.n.baker@gmail.com>
2023-05-17 14:59:37 -0700

    Cleanup theme files (#1)
    * remove husky from repo
    
    * get devshell working
    
    * cleaned up flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "djacu's personal site";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.npmlock2nix.url = "github:nix-community/npmlock2nix";
+  inputs.npmlock2nix.flake = false;
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    npmlock2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (self: super: {
+              npmlock2nix = pkgs.callPackage npmlock2nix {};
+            })
+          ];
+        };
+
+        astro-shell = pkgs.npmlock2nix.v2.shell {
+          src = ./.;
+          nodejs = pkgs.nodejs;
+          node_modules_mode = "copy";
+
+        };
+      in {
+        devShells.astro = astro-shell;
+      }
+    );
+}
