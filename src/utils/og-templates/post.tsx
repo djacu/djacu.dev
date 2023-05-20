e3b43a1fae3b97655dd23d36301b8e8bbc4dbfe5
Dan Baker <daniel.n.baker@gmail.com>
2023-05-19 23:58:28 -0700

    blog/update og image styling (#4)
    * fixed arbitrary font fetching. fixed font file imports
    
    * og image looks a little nicer
    
    * got the og image using a background wave image
    
    * got the rest of the blog frontmatter available for the OG image and injected the description

--- src/utils/og-templates/post.tsx
+++ src/utils/og-templates/post.tsx
@@ -1,96 +1,123 @@
+import * as fs from 'fs';
+
 import { SITE } from "@config";
 import type { CollectionEntry } from "astro:content";
 
-export default (post: CollectionEntry<"blog">) => {
-  return (
-    <div
-      style={{
-        background: "#fefbfb",
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          top: "-1px",
-          right: "-1px",
-          border: "4px solid #000",
-          background: "#ecebeb",
-          opacity: "0.9",
-          borderRadius: "4px",
-          display: "flex",
-          justifyContent: "center",
-          margin: "2.5rem",
-          width: "88%",
-          height: "80%",
-        }}
-      />
+const { readFile } = fs.promises;
 
-      <div
-        style={{
-          border: "4px solid #000",
-          background: "#fefbfb",
-          borderRadius: "4px",
-          display: "flex",
-          justifyContent: "center",
-          margin: "2rem",
-          width: "88%",
-          height: "80%",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            margin: "20px",
-            width: "90%",
-            height: "90%",
-          }}
-        >
-          <p
-            style={{
-              fontSize: 72,
-              fontWeight: "bold",
-              maxHeight: "84%",
-              overflow: "hidden",
-            }}
-          >
-            {post.data.title}
-          </p>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "100%",
-              marginBottom: "8px",
-              fontSize: 28,
-            }}
-          >
-            <span>
-              by{" "}
-              <span
-                style={{
-                  color: "transparent",
-                }}
-              >
-                "
-              </span>
-              <span style={{ overflow: "hidden", fontWeight: "bold" }}>
-                {post.data.author}
-              </span>
-            </span>
-
-            <span style={{ overflow: "hidden", fontWeight: "bold" }}>
-              {SITE.title}
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+const fetchBackground = async () => {
+  const mime = 'image/png';
+  const encoding = 'base64';
+  const data = await readFile('static/imgs/waves.png');
+  const dataBase64 = data.toString(encoding);
+  return  `data:${mime};${encoding},${dataBase64}`;
 };
+
+export default (post: CollectionEntry<"blog">) => {
+    return async () => {
+        const bgImage = await fetchBackground();
+        return (
+            <div
+                style={{
+                    backgroundImage: `url(${bgImage})`,
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginBottom: "2rem",
+                        width: "88%",
+                        height: "90%",
+                    }}
+                >
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            margin: "20px",
+                            width: "90%",
+                            height: "90%",
+                        }}
+                    >
+                        <p
+                            style={{
+                                fontSize: 72,
+                                fontWeight: "bold",
+                                color: "#DEE5ED",
+                                maxHeight: "84%",
+                                overflow: "hidden",
+                                textShadow: "2px 2px #3A5069",
+                            }}
+                        >
+                            {post.data.title}
+                        </p>
+                        <p
+                            style={{
+                                position: "absolute",
+                                top: "50%",
+                                fontSize: 28,
+                                fontWeight: "bold",
+                                color: "#DEE5ED",
+                                maxHeight: "84%",
+                                overflow: "hidden",
+                                textShadow: "2px 2px #3A5069",
+                            }}
+                        >
+                            {post.data.description}
+                        </p>
+                        <div
+                            style={{
+                                position: "absolute",
+                                bottom: "0",
+                                display: "flex",
+                                justifyContent: "space-between",
+                                width: "100%",
+                                marginBottom: "8px",
+                                fontSize: 28,
+                                color: "#DEE5ED",
+                            }}
+                        >
+                            <span
+                                style={{
+                                    textShadow: "2px 2px #3A5069",
+                                }}
+                            >
+                                by{" "}
+                                <span
+                                    style={{
+                                        color: "transparent",
+                                    }}
+                                >
+                                    "
+                                </span>
+                                <span style={{
+                                    overflow: "hidden",
+                                    fontWeight: "bold",
+                                    color: "#DEE5ED",
+                                    textShadow: "2px 2px #3A5069",
+                                }}>
+                                    {post.data.author}
+                                </span>
+                            </span>
+
+                            <span style={{
+                                overflow: "hidden",
+                                fontWeight: "bold",
+                                textShadow: "2px 2px #3A5069",
+                            }}>
+                                {SITE.title}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+    
+            </div>
+        );
+    };
+}
