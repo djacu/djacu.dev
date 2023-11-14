04b165f2370269c403028dcabc8c31ed25ddcce1
Daniel Baker <daniel.baker@tweag.io>
2023-11-13 18:49:36 -0800

    Possible fix for the post og-template.

--- src/utils/og-templates/post.tsx
+++ src/utils/og-templates/post.tsx
@@ -13,111 +13,110 @@ const fetchBackground = async () => {
   return  `data:${mime};${encoding},${dataBase64}`;
 };
 
+const bgImage = await fetchBackground();
+
 export default (post: CollectionEntry<"blog">) => {
-    return async () => {
-        const bgImage = await fetchBackground();
-        return (
+    return (
+        <div
+            style={{
+                backgroundImage: `url(${bgImage})`,
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
             <div
                 style={{
-                    backgroundImage: `url(${bgImage})`,
-                    width: "100%",
-                    height: "100%",
                     display: "flex",
-                    alignItems: "center",
                     justifyContent: "center",
+                    marginBottom: "2rem",
+                    width: "88%",
+                    height: "90%",
                 }}
             >
                 <div
                     style={{
                         display: "flex",
-                        justifyContent: "center",
-                        marginBottom: "2rem",
-                        width: "88%",
+                        flexDirection: "column",
+                        margin: "20px",
+                        width: "90%",
                         height: "90%",
                     }}
                 >
-                    <div
+                    <p
                         style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            margin: "20px",
-                            width: "90%",
-                            height: "90%",
+                            fontSize: 72,
+                            fontWeight: "bold",
+                            color: "#DEE5ED",
+                            maxHeight: "84%",
+                            overflow: "hidden",
+                            textShadow: "2px 2px #3A5069",
                         }}
                     >
-                        <p
+                        {post.data.title}
+                    </p>
+                    <p
+                        style={{
+                            position: "absolute",
+                            top: "50%",
+                            fontSize: 28,
+                            fontWeight: "bold",
+                            color: "#DEE5ED",
+                            maxHeight: "84%",
+                            overflow: "hidden",
+                            textShadow: "2px 2px #3A5069",
+                        }}
+                    >
+                        {post.data.description}
+                    </p>
+                    <div
+                        style={{
+                            position: "absolute",
+                            bottom: "0",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            width: "100%",
+                            marginBottom: "8px",
+                            fontSize: 28,
+                            color: "#DEE5ED",
+                        }}
+                    >
+                        <span
                             style={{
-                                fontSize: 72,
-                                fontWeight: "bold",
-                                color: "#DEE5ED",
-                                maxHeight: "84%",
-                                overflow: "hidden",
                                 textShadow: "2px 2px #3A5069",
                             }}
                         >
-                            {post.data.title}
-                        </p>
-                        <p
-                            style={{
-                                position: "absolute",
-                                top: "50%",
-                                fontSize: 28,
-                                fontWeight: "bold",
-                                color: "#DEE5ED",
-                                maxHeight: "84%",
-                                overflow: "hidden",
-                                textShadow: "2px 2px #3A5069",
-                            }}
-                        >
-                            {post.data.description}
-                        </p>
-                        <div
-                            style={{
-                                position: "absolute",
-                                bottom: "0",
-                                display: "flex",
-                                justifyContent: "space-between",
-                                width: "100%",
-                                marginBottom: "8px",
-                                fontSize: 28,
-                                color: "#DEE5ED",
-                            }}
-                        >
+                            by{" "}
                             <span
                                 style={{
-                                    textShadow: "2px 2px #3A5069",
+                                    color: "transparent",
                                 }}
                             >
-                                by{" "}
-                                <span
-                                    style={{
-                                        color: "transparent",
-                                    }}
-                                >
-                                    "
-                                </span>
-                                <span style={{
-                                    overflow: "hidden",
-                                    fontWeight: "bold",
-                                    color: "#DEE5ED",
-                                    textShadow: "2px 2px #3A5069",
-                                }}>
-                                    {post.data.author}
-                                </span>
+                                "
                             </span>
-
                             <span style={{
                                 overflow: "hidden",
                                 fontWeight: "bold",
+                                color: "#DEE5ED",
                                 textShadow: "2px 2px #3A5069",
                             }}>
-                                {SITE.title}
+                                {post.data.author}
                             </span>
-                        </div>
+                        </span>
+
+                        <span style={{
+                            overflow: "hidden",
+                            fontWeight: "bold",
+                            textShadow: "2px 2px #3A5069",
+                        }}>
+                            {SITE.title}
+                        </span>
                     </div>
                 </div>
-    
             </div>
-        );
-    };
+
+        </div>
+    );
 }
